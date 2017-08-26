67e1a2221de459df2c3e9294c605ae4ed5743f59
Baitu Huang <baitu.huang@shopeemobile.com>
2017-08-27 00:57:54 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# redux-less
-less code with redux
+## redux-less
+
+write a redux reducer with less code, inspired by [mirror](https://github.com/mirrorjs/mirror)
+
+### example
+
+```javascript
+import getReducer from 'redux-less'
+const reducer = getReducer({
+  key: 'todo',
+  initialState: [],
+  getTodoList(dispatch, getState, other) {
+    const action = () => {
+      fetch('./api/todos')
+        .then(res => res.json())
+        .then(list => dispatch(list))
+    }
+    const reducer = (state, action) => {
+      return state.concat(action.payload)
+    }
+  },
+  addTodo(state, action) {
+    return state.concat(action.payload)
+  },
+  removeTodo(state, action) {
+    const todos = state.slice()
+    todos.splice(action.payload, 1)
+    return todos
+  },
+})
+
+// for react-redux, binding to container
+export default connect(state => ({
+  todoList: state[reducer.key]
+}), reducer.actions)(props => {
+  // ...
+  <button onClick={() => this.props.addTodo('new item')}>add</button>
+})
+
+```
+
+### license
+MIT
---
+++ index.js
@@ -0,0 +1,61 @@
+import { bindActionCreators } from 'redux'
+
+const ACTIONS = Object.create(null)
+
+const SPLIT = ' ^_^ '
+
+let _dispatch = () => {
+  throw new Error('you have to apply middleware when you create the store')
+}
+const exportDispathMiddleware = ({ dispatch, getState }) => {
+  _dispatch = dispatch
+  return next => action => {
+    return next(action)
+  }
+}
+
+function getReducer(config) {
+  const actions = {}
+
+  for (let actionName in config) {
+    if (actionName === 'key' || actionName === 'initialState') continue
+    const type = config.key + SPLIT + actionName
+    const reducer = config[actionName]
+    if (typeof reducer === 'function' && reducer.length <= 2) {
+      actions[actionName] = payload => ({ type, payload })
+    } else {
+      // support redux-thunk
+      actions[actionName] = (...args) => (dispatch, getState, extraArgument) => {
+        if (typeof reducer === 'object') {
+          return reducer.action(...args)
+        }
+        config[actionName] = reducer(
+          // support Flux Standard Action
+          (payload, error, meta) => dispatch({ type, payload, error, meta }),
+          (key = config.key) => getState()[key],
+          extraArgument
+        )
+        return config[actionName].action(...args) // action contains async code
+      }
+    }
+  }
+  function reducer(state = config.initialState, action) {
+    const handlerName = action.type.split(SPLIT)[1]
+    if (!(handlerName in config)) return state
+    let reducerFunc = config[handlerName]
+    if (typeof reducerFunc === 'object') {
+      reducerFunc = reducerFunc.reducer
+    }
+    return reducerFunc(state, action)
+  }
+  reducer.key = config.key
+  reducer.actions = actions
+  ACTIONS[reducer.key] = bindActionCreators(actions, _dispatch)
+  return reducer
+}
+
+export {
+  ACTIONS as actions,
+  getReducer as default,
+  exportDispathMiddleware
+}
