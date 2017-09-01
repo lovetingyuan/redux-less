14276746c9e6de397efb2c8e8e3f312c6e07d395
Baitu Huang <baitu.huang@shopeemobile.com>
2017-09-01 10:50:41 +0800

    add min version

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 
-module.exports = {
+const env = process.env.NODE_ENV
+
+const config = {
   input: './src/index.js',
   output: {
     exports: 'named',
@@ -10,15 +13,33 @@ module.exports = {
     format: 'umd',
   },
   name: 'ReduxLess',
-  sourcemap: false,
+  sourcemap: true,
   plugins: [
-    resolve(),
+    resolve({
+      jsnext: true
+    }),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify(env)
     })
   ],
   // external: ['lodash-es', 'redux'],
 };
+
+if (env === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  )
+  config.output.file = './dist/ReduxLess.min.js'
+}
+
+module.exports = config
